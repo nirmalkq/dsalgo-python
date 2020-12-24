7463ed48e3c0631c2e01e98c426cdb65bc7c0230
Nirmal Kumar <51245535+nirmalkq@users.noreply.github.com>
2020-12-24 14:40:15 +0530

    Create FirstRecurringChar.py

---
+++ FirstRecurringChar.py
@@ -0,0 +1,14 @@
+class Solution:
+    def first_recurring_char(self,str):
+        counts = {}
+        for char in str:
+            if char in counts:
+                return char
+            else:
+                counts[char] = 1
+        return None
+
+
+sl = Solution()
+str = "BCDAEFD"
+print(sl.first_recurring_char(str))
